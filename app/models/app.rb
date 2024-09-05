266cbf3639da44a44d6d18bac45dc0d4c4d48fd6
Vinay@itworks <vinay.poul@infotech.works>
2024-09-05 19:06:07 +0530

    Established relationship between App, Customer and Review.

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,2 +1,4 @@
 class App < ApplicationRecord
+  has_many :customers, through: :reviews
+  has_many :reviews
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,4 @@
 class Customer < ApplicationRecord
+  has_many :apps, through: :reviews
+  has_many :reviews
 end
